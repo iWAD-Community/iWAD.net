cd66cf60c76b427ed293db3bb56717715252bd80
iWAD-Community <66331537+iWAD-Community@users.noreply.github.com>
2021-02-04 20:05:37 +0200

    home to index

--- original doc/About us.php
+++ original doc/About us.php
@@ -33,7 +33,7 @@
           
               <div class="nav__menu" id="nav-menu">
                   <ul class="nav__list">
-                      <li class="nav__item"><a href="home.php" class="nav__link">Home</a></li>
+                      <li class="nav__item"><a href="index.php" class="nav__link">Home</a></li>
                       <li class="nav__item"><a href="Services.php" class="nav__link">Services</a></li>
                       <li class="nav__item"><a href="About us.php" class="nav__link active-link">About Us</a></li>
                       <li class="nav__item"><a href="iwad Store.php" class="nav__link">iWAD Store</a></li>
--- original doc/includes/login.inc.php
+++ original doc/includes/login.inc.php
@@ -8,14 +8,14 @@ if(isset($_POST['login-submit'])){
     $password = $_POST['pwd'];
 
     if (empty($mailuid) || empty($password)) {
-      header("Location: ../home.php?error=emptyfields");
+      header("Location: ../index.php?error=emptyfields");
       exit();
     }
     else {
       $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
       $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("Location: ../home.php?error=sqlerror");
+        header("Location: ../index.php?error=sqlerror");
         exit();
       }
 
@@ -28,7 +28,7 @@ if(isset($_POST['login-submit'])){
         if ($row = mysqli_fetch_assoc($result)) {
           $pwdCheck = password_verify($password, $row['pwdUsers']);
           if ($pwdCheck == false) {
-            header("Location: ../home.php?error=wrongpwd");
+            header("Location: ../index.php?error=wrongpwd");
             exit();
           }
 
@@ -37,12 +37,12 @@ if(isset($_POST['login-submit'])){
             $_SESSION['userId'] = $row['idUsers'];
             $_SESSION['userUid'] = $row['uidUsers'];
 
-            header("Location: ../home.php?login=success");
+            header("Location: ../index.php?login=success");
              exit();
           }
         }
         else {
-         header("Location: ../home.php?error=wrongpwd");
+         header("Location: ../index.php?error=wrongpwd");
           exit();
         }
       }
