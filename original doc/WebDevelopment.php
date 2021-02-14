18579c222a20097ffb8842cbeb7aebb6e860ea81
TatendaAndreaMangere <65562550+TatendaAndreaMangere@users.noreply.github.com>
2021-02-14 23:40:53 +0200

    sa

--- original doc/WebDevelopment.php
+++ original doc/WebDevelopment.php
@@ -57,7 +57,7 @@
 
 
 
-              
+              <div class="share__container bd-grid">
         <div class="share__data">
             <h2 class="section-title-center">| Web Development  |  </h2>
             <p class="share__description">  Our web developers dedicate themselves to the highest quality engineering. Python, Javascript frameworks,.Net, HTML, CSS and React,PHP, Angular and beyond. The team delivers remarkable fully responsive websites that will outdo your competition. Our web applications boast beauty and brains, with gorgeous design, cutting-edge code, exceptional usability, and conversion strategy built right into each and every one. iWAD has extensive experience creating innovative Websites  with different technologies.</p>
@@ -69,7 +69,16 @@
         </div>
     </div>
     
- 
+    <div class="share__container bd-grid">
+        <div class="share__data">
+            <h2 class="section-title-center"> Custom Web development |  <h4>What Ever You May Desire, we  are here</h4>  </h2>
+            <p class="share__description"> We build sites that work for your business and your users — be it a complex intranet platform, a progressive web app, or a marketing site we can deliver.A one size fits all solution sometimes does not fit the individual needs, wants and goals of every organization. Custom web applications can solve problems within an organization, bring an exciting new idea to your customers, better promote your brand and provide increased security. Unleashed Technologies builds powerful and scalable web apps that leverage the latest web technologies and agile development processes. </p>
+          
+        </div>
+
+      
+    </div>
+</section>
         <!--========== Start OUR SERVICES ==========-->
 
   <section class="decoration section bd-container" id="decoration">
