0064cdee81b151188f62a2bfc188095e0fd0b399
TatendaAndreaMangere <65562550+TatendaAndreaMangere@users.noreply.github.com>
2021-02-14 23:23:32 +0200

    update

--- original doc/WebDevelopment.php
+++ original doc/WebDevelopment.php
@@ -54,23 +54,32 @@
   
           
        
-</section>
-<div class = "head-body container">
-                <div class = "head-body-content">
-                <h2 class="section-title-center"> | Web Development  |  </h2>|</h4>
-                 
-                Our web developers dedicate themselves to the highest quality engineering. Python, Javascript frameworks,.Net, HTML, CSS and React,PHP, Angular and beyond. The team delivers remarkable fully responsive websites that will outdo your competition. Our web applications boast beauty and brains, with gorgeous design, cutting-edge code, exceptional usability, and conversion strategy built right into each and every one. iWAD has extensive experience creating innovative Websites  with different technologies.
-                  </p>
-                 
-                </div>
-          
-                <div class = "head-body-img">
-                  <img src = "./assets/img/send1.png" alt = "header image">
-                </div>
-                  
 
-              </div>
-         
+
+
+              <div class="share__container bd-grid">
+        <div class="share__data">
+            <h2 class="section-title-center">| Web Development  |  </h2>
+            <p class="share__description">  Our web developers dedicate themselves to the highest quality engineering. Python, Javascript frameworks,.Net, HTML, CSS and React,PHP, Angular and beyond. The team delivers remarkable fully responsive websites that will outdo your competition. Our web applications boast beauty and brains, with gorgeous design, cutting-edge code, exceptional usability, and conversion strategy built right into each and every one. iWAD has extensive experience creating innovative Websites  with different technologies.</p>
+          
+        </div>
+
+        <div class="share__img">
+            <img src="assets/img/send1.png" alt="">
+        </div>
+    </div>
+    
+    <div class="share__container bd-grid">
+        <div class="share__data">
+            <h2 class="section-title-center"> Custom Web development |  <h4>What Ever You May Desire, we  are here</h4>  </h2>
+            <p class="share__description"> We build sites that work for your business and your users — be it a complex intranet platform, a progressive web app, or a marketing site we can deliver.A one size fits all solution sometimes does not fit the individual needs, wants and goals of every organization. Custom web applications can solve problems within an organization, bring an exciting new idea to your customers, better promote your brand and provide increased security. Unleashed Technologies builds powerful and scalable web apps that leverage the latest web technologies and agile development processes. </p>
+          
+        </div>
+
+        <div class="share__img">
+            <img src="assets/img/send1.png" alt="">
+        </div>
+    </div>
 
         <!--========== Start OUR SERVICES ==========-->
 
